f73c71b3ae0f215e27eeef77fd1c5b3bd1c16327
Mooses Cachopas <mcachopas@gmail.com>
2023-11-14 20:22:39 -0300

    Commit Mooses
    Commit Mooses

---
+++ While and List.py
@@ -0,0 +1,11 @@
+print("Digite apenas numeros positivos \n Em caso de numeros negativos o sistema sairá")
+
+InputVariable = 0.0
+InputVariablestring = ""
+VariableList = []
+while InputVariable >= 0:
+    InputVariablestring = input("Digite o próximo Numero")
+    is_float("")
+    if (is_float(InputVariablestring))
+    VariableList.append(InputVariable)
+print (f"Estes são os numeros digitados {VariableList}")
