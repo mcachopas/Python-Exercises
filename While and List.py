51c36e01276200d2f05bf7e4cc086930198ce920
Mooses Cachopas <mcachopas@gmail.com>
2023-11-14 20:37:57 -0300

    Fim do Exercicio 1

--- While and List.py
+++ While and List.py
@@ -1,11 +1,14 @@
-print("Digite apenas numeros positivos \n Em caso de numeros negativos o sistema sairá")
-
 InputVariable = 0.0
 InputVariablestring = ""
 VariableList = []
+
+print("Digite apenas numeros positivos \nEm caso de numeros negativos o sistema sairá")
 while InputVariable >= 0:
     InputVariablestring = input("Digite o próximo Numero")
-    is_float("")
-    if (is_float(InputVariablestring))
-    VariableList.append(InputVariable)
+    try:
+        float(InputVariablestring)
+        InputVariable = float(InputVariablestring)
+        VariableList.append(InputVariable)
+    except ValueError:
+        print ("Valor não é um Numero")
 print (f"Estes são os numeros digitados {VariableList}")
